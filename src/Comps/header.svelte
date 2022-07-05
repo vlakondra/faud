a3396312ef1145584fde27a9d0fe7b09e77a9a77
vlakondra <kondrashkin.wladimir@yandex.ru>
2022-07-05 23:43:40 +0500

    begin header

---
+++ src/Comps/header.svelte
@@ -0,0 +1,104 @@
+<script>
+    // import { time } from "./store.js";
+    let time = new Date();
+    export let onBurgerClick;
+
+    import Fa from "svelte-fa";
+    import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+    // https://cweili.github.io/svelte-fa/
+    //https://fontawesome.com/v5/icons/bars?s=solid
+
+    const formatter = new Intl.DateTimeFormat("ru", {
+        hour12: false,
+        hour: "numeric",
+        minute: "2-digit",
+        second: "2-digit",
+    });
+</script>
+
+<div id="header" class="header">
+    <div class="date">
+        {new Date().toLocaleDateString("ru-RU")}
+    </div>
+    <div class="caption-wrapp">
+        <div class="kv-burger" on:click={onBurgerClick}>
+            <Fa icon={faBars} />
+        </div>
+        <div class="caption">Расписание студенческих групп</div>
+    </div>
+    <div class="time">{formatter.format(time)}</div>
+</div>
+
+<style lang="scss">
+    .header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        background-color: #bea5f5;
+        flex-wrap: nowrap;
+        align-content: center;
+        justify-content: space-around;
+        padding: 10px 0;
+        margin-top: 10px;
+        margin-bottom: 25px;
+        box-shadow: 1px 2px 6px 0px #5a3c3c;
+        border-radius: 5px;
+    }
+    .date,
+    .time {
+        font-size: 1.15em;
+        font-weight: 400;
+        text-shadow: 1px 1px 1px rgb(75 50 31);
+        color: #f9ffef;
+        letter-spacing: 0.5px;
+    }
+
+    .kv-burger {
+        width: 50px;
+        text-align: center;
+        color: #fff;
+        cursor: pointer;
+    }
+    .caption {
+        text-shadow: 1px 1px 1px rgb(75 50 31);
+        color: #fff;
+        letter-spacing: 1px;
+        line-height: 1em;
+        font-weight: 100;
+    }
+    .caption-wrapp {
+        display: flex;
+        flex-wrap: nowrap;
+        align-items: center;
+    }
+    // Перепроверить на мобильнике
+    @media (min-width: 500px) {
+        .kv-burger {
+            font-size: 2em;
+        }
+        .caption {
+            font-size: 1.85em;
+        }
+    }
+
+    @media (max-width: 500px) {
+        .header {
+            padding: 5px 5px;
+            margin: 3px 0 0px 0;
+        }
+        .caption {
+            font-size: 5.5vw;
+        }
+        .kv-burger {
+            font-size: 1.75em;
+        }
+    }
+
+    @media (max-width: 700px) {
+        .date,
+        .time {
+            display: none;
+        }
+    }
+</style>
